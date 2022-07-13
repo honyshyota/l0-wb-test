64c5dc2060cd2fe4dc0e06c13abef869be64651c
honyshyota <semensuprunenko@gmail.com>
2022-07-13 18:49:47 +0300

    ♻️ change id from string to int

--- internal/model/model.go
+++ internal/model/model.go
@@ -3,55 +3,56 @@ package model
 import "time"
 
 type Order struct {
-	OrderUID          string `json:"order_uid"`
-	TrackNum          string `json:"track_number"`
-	Entry             string `json:"entry"`
-	Delivery          *Delivery
-	Payment           *Payment
-	Items             *[]Item
-	Locale            string    `json:"locale"`
-	InternalSignature string    `json:"internal_signature"`
-	CustomerID        string    `json:"customer_id"`
-	DeliveryService   string    `json:"delivery_service"`
-	ShardKey          string    `json:"shard_key"`
-	SmID              int       `json:"sm_id"`
-	DateCreated       time.Time `json:"date_created"`
-	OofShard          string    `json:"oof_shard"`
+	ID                int       `json:"id" db:"id"`
+	OrderUID          string    `json:"order_uid" db:"order_uid"`
+	TrackNum          string    `json:"track_number" db:"track_number"`
+	Entry             string    `json:"entry" db:"entry"`
+	Delivery          *Delivery `db:"delivery"`
+	Payment           *Payment  `db:"payment"`
+	Items             *[]Item   `db:"items"`
+	Locale            string    `json:"locale" db:"locale"`
+	InternalSignature string    `json:"internal_signature" db:"internal_signature"`
+	CustomerID        string    `json:"customer_id" db:"customer_id"`
+	DeliveryService   string    `json:"delivery_service" db:"delivery_service"`
+	ShardKey          string    `json:"shard_key" db:"shard_key"`
+	SmID              int       `json:"sm_id" db:"sm_id"`
+	DateCreated       time.Time `json:"date_created" db:"date_created"`
+	OofShard          string    `json:"oof_shard" db:"oof_shard"`
 }
 
 type Delivery struct {
-	Name    string `json:"name"`
-	Phone   string `json:"phone"`
-	Zip     string `json:"zip"`
-	City    string `json:"city"`
-	Address string `json:"address"`
-	Region  string `json:"region"`
-	Email   string `json:"email"`
+	Name    string `json:"name" db:"name"`
+	Phone   string `json:"phone" db:"phone"`
+	Zip     string `json:"zip" db:"zip"`
+	City    string `json:"city" db:"city"`
+	Address string `json:"address" db:"address"`
+	Region  string `json:"region" db:"region"`
+	Email   string `json:"email" db:"email"`
 }
 
 type Payment struct {
-	Transaction  string `json:"transaction"`
-	RequestID    string `json:"request_id"`
-	Currency     string `json:"currency"`
-	Provider     string `json:"provider"`
-	Amount       int    `json:"amount"`
-	PaymentDT    int    `json:"payment_dt"`
-	Bank         string `json:"bank"`
-	DeliveryCost int    `json:"delivery_cost"`
-	GoodsTotal   int    `json:"goods_total"`
-	CustomFee    int    `json:"custom_fee"`
+	Transaction  string `json:"transaction" db:"transaction"`
+	RequestID    string `json:"request_id" db:"request_id"`
+	Currency     string `json:"currency" db:"currency"`
+	Provider     string `json:"provider" db:"provider"`
+	Amount       int    `json:"amount" db:"amount"`
+	PaymentDT    int    `json:"payment_dt" db:"payment_dt"`
+	Bank         string `json:"bank" db:"bank"`
+	DeliveryCost int    `json:"delivery_cost" db:"delivery_cost"`
+	GoodsTotal   int    `json:"goods_total" db:"goods_total"`
+	CustomFee    int    `json:"custom_fee" db:"custom_fee"`
 }
 
 type Item struct {
-	ChrtID      int    `json:"chrt_id"`
-	TrackNumber string `json:"track_number"`
-	Price       int    `json:"price"`
-	Rid         string `json:"rid"`
-	Name        string `json:"name"`
-	Sale        int    `json:"sale"`
-	Size        string `json:"size"`
-	TotalPrice  int    `json:"total_price"`
-	NmID        int    `json:"nm_id"`
-	Brand       string `json:"brand"`
-	Status      int    `json:"status"`
+	ChrtID      int    `json:"chrt_id" db:"chrt_id"`
+	TrackNumber string `json:"track_number" db:"track_number"`
+	Price       int    `json:"price" db:"price"`
+	Rid         string `json:"rid" db:"rid"`
+	Name        string `json:"name" db:"name"`
+	Sale        int    `json:"sale" db:"sale"`
+	Size        string `json:"size" db:"size"`
+	TotalPrice  int    `json:"total_price" db:"total_price"`
+	NmID        int    `json:"nm_id" db:"nm_id"`
+	Brand       string `json:"brand" db:"brand"`
+	Status      int    `json:"status" db:"status"`
 }
